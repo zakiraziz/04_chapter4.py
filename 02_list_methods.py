514cc69c012eb591fd2699212652913c12e0d48e
zakir <azizizakir449@gmail.com>
2024-08-17 18:48:33 +0500

    my commit

--- 02_list_methods.py
+++ 02_list_methods.py
@@ -6,7 +6,7 @@ print(friends)
 l1 = [1, 34,62, 2, 6, 11]
 # l1.sort()
 # l1.reverse()
-l1.insert(3, 333333) # Insert3, 333333such that its index in the list is 3
+#l1.insert(3, 333333) # Insert3, 333333such that its index in the list is 3
 value = l1.pop(3)
 l1.pop(value)
 print(l1)
---
+++ 02_problem2.py
@@ -0,0 +1,18 @@
+marks = []
+
+f1 = input("Enter marks here: ")
+marks.append(f1)
+f2 = input("Enter marks here: ")
+marks.append(f2)
+f3 = input("Enter marks here: ")
+marks.append(f3)
+f4 = input("Enter marks here: ")
+marks.append(f4)
+f5 = input("Enter marks here: ")
+marks.append(f5)
+f6 = input("Enter marks here: ")
+marks.append(f6)
+
+marks.sort()
+
+print(marks)
